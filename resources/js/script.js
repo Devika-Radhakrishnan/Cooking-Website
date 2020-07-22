409174c1ffbe0cd34d22139346f94fc13c8f67c9
devika <devika.radhakrishnan.g@gmail.com>
2020-07-23 00:23:13 +0530

    mob navigation added

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,86 @@
+
+
 $(document).ready(function () {
-    $('.js--main-section').waypoint(function (direction) {
+
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
-            $('navbar').addClass('sticky');
+            $('nav').addClass('sticky');
         } else {
-            $('navbar').removeClass('sticky');
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+
+
+
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+    });
+
+    // navigation 
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1500);
+                return false;
+            }
         }
     });
 
+
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
+
+
 });
+
+
+
+
+
+
+
+
+
+// another way for sticky
+
+// jQuery(document).ready(function () {
+//     var navOffset = jQuery("nav").offset().top;
+
+//     jQuery(window).scroll(function () {
+//         var scrollPos = jQuery(window).scrollTop();
+//         // jQuery(".status").html(scrollPos);
+//         if (scrollPos > navOffset) {
+//             jQuery('nav').addClass('sticky');
+//         } else {
+//             jQuery('nav').removeClass('sticky');
+//         }
+
+//     });
+
+// });
